c0895f83a2e4493e0d9fafb81288eb48416b407c
Katsuraji1 <verahovets@gmail.com>
2023-04-15 19:02:34 +0300

    fixed userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,15 @@
+const userService = require("../service/userService");
+
 class UserContoller {
     async registration (req, res, next) {
         try{
-
+            const {email, password} = req.body;
+            const userData = await userService.registration(password, email)
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+            return res.json(userData)
         } 
         catch(e) {
-
+            console.log(e)
         }
     }
 
