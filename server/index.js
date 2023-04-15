22866e508c223bf2876e9f7da738239df409de99
Katsuraji1 <verahovets@gmail.com>
2023-04-15 13:36:04 +0300

    created server

--- server/index.js
+++ server/index.js
@@ -1,2 +1,19 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
 const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+
+const PORT = process.env.PORT
+
+const start = async () => {
+    try {
+        app.listen(PORT, () => console.log(`${PORT}`))
+    } 
+    catch(e) {
+        console.log(e)
+    }
+}
+
+start()
