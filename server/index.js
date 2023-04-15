bef2b3f76dcd7ff5eb6f380d81f816505540b795
Katsuraji1 <verahovets@gmail.com>
2023-04-15 14:36:22 +0300

    connected to mongodb

--- server/index.js
+++ server/index.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const mongoose = require('mongoose')
 require('dotenv').config()
 
 const app = express()
+
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json())
 
 const PORT = process.env.PORT
 
 const start = async () => {
     try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
         app.listen(PORT, () => console.log(`${PORT}`))
     } 
     catch(e) {
