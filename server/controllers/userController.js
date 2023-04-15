a81a1fd67212ee20a731124e560789909d16e5b0
Katsuraji1 <verahovets@gmail.com>
2023-04-15 16:41:31 +0300

    created controllers

---
+++ server/controllers/userController.js
@@ -0,0 +1,55 @@
+class UserContoller {
+    async registration (req, res, next) {
+        try{
+
+        } 
+        catch(e) {
+
+        }
+    }
+
+    async login (req, res, next) {
+        try{
+
+        } 
+        catch(e) {
+            
+        }
+    }
+
+    async logout (req, res, next) {
+        try{
+
+        } 
+        catch(e) {
+            
+        }
+    }
+
+    async refresh (req, res, next) {
+        try{
+
+        } 
+        catch(e) {
+            
+        }
+    }
+
+    async users (req, res, next) {
+        try{
+            res.json(['1','1'])
+        } 
+        catch(e) {
+            
+        }
+    }
+
+    async activate (req, res, next) {
+        try{
+
+        } 
+        catch(e) {
+            
+        }
+    }
+}
