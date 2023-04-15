ff8a8780a9ac4395e02a4b3b6ae288876731a84f
Katsuraji1 <verahovets@gmail.com>
2023-04-15 16:45:14 +0300

    connected user  controller

--- server/router/index.js
+++ server/router/index.js
@@ -1,10 +1,14 @@
+const userController = require('../controllers/userController')
+
 const Router = require('express').Router
 
 const router = new Router()
 
-router.post('/registration')
-router.post('/login')
-router.post('/logout')
-router.get('/activate/:link')
-router.get('/users')
-router.get('/refresh')
+router.post('/registration', userController.registration)
+router.post('/login', userController.login)
+router.post('/logout', userController.logout)
+router.get('/activate/:link', userController.activate)
+router.get('/users', userController.users)
+router.get('/refresh', userController.refresh)
+
+module.exports = router
