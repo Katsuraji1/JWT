48b6b73f48ea168631c022101d1f727ea9599691
Katsuraji1 <verahovets@gmail.com>
2023-04-16 14:52:42 +0300

    email and password validator

--- server/router/index.js
+++ server/router/index.js
@@ -1,10 +1,11 @@
 const userController = require('../controllers/userController')
+const {body} = require('express-validator')
 
 const Router = require('express').Router
 
 const router = new Router()
 
-router.post('/registration', userController.registration)
+router.post('/registration',body('email').isEmail(), body('password').isLength({min:4, max: 16}) ,userController.registration)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 router.get('/activate/:link', userController.activate)
