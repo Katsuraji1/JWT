6f5f2abed219599f32d4798fb43efe8c95b4104a
Katsuraji1 <verahovets@gmail.com>
2023-04-16 14:46:07 +0300

    created mail service

--- server/service/mailService.js
+++ server/service/mailService.js
@@ -1,7 +1,33 @@
-
+require('dotenv').config()
+const nodemailer = require('nodemailer')
 class MailService {
-    async sendActivationLink(to, link) {
 
+    constructor() {
+        this.transparent = nodemailer.createTransport({
+            secure: false,
+            port: process.env.MAIL_PORT,
+            host: process.env.MAIL_HOST,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASSWORD,
+            },
+        })
+    }
+
+    async sendActivationLink(to, link) {
+        this.transparent.sendMail({
+            from: process.env.MAIL_USER,
+            to,
+            text: '',
+            subject: 'Авторизация',
+            html:
+                `
+                <div>
+                    <h1>Авторизация на ${process.env.MAIN_HOST}</h1>
+                    <a href = ${link}>Подтвердить</a>
+                </div>
+                `
+        })
     } 
 }
 
