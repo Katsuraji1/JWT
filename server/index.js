6fefecde59a421c73b980539e0402f7fa7f645fd
Katsuraji1 <verahovets@gmail.com>
2023-04-17 14:45:58 +0300

    fixed cors

--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ require('dotenv').config()
 
 const app = express()
 
-app.use(cors())
+app.use(cors(
+    {
+        credentials: true,
+        origin: process.env.FRONTEND_HOST
+    }
+))
 app.use(cookieParser())
 app.use(express.json())
 app.use('/api', router)
